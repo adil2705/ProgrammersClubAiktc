91594e8f1481378d472f9c3d4068c086b6f58d3f
arman <ak2341776@gmail.com>
2024-01-25 15:34:38 +0530

    Update routes and signup page UI

--- src/algorithm8.0/pages/AlgoHome.jsx
+++ src/algorithm8.0/pages/AlgoHome.jsx
@@ -8,10 +8,10 @@ export default function AlgoHome() {
           >
             <a
               className="focus:outline-none inline-block transform rounded-lg my-6 bg-green-400 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-green-600 focus:ring focus:ring-green-400 focus:ring-opacity-50 focus:ring-offset-2 active:bg-green-400 sm:text-base"
-              href="/signuplogin"
+              href="/signup"
               target="_self"
             >
-              SignupLogin
+              Signup
             </a>
           </div>
         </div>
--- src/algorithm8.0/pages/Signup.jsx
+++ src/algorithm8.0/pages/Signup.jsx
@@ -1,31 +1,44 @@
 import React from "react";
+import Lottie from 'react-lottie';
+import SigninAnimation from '../assets/lottie/signin.json'
 
 export default function Signup() {
-    return (
-        <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-          <div className="md:w-1/3 max-w-sm">
-            <img
-              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              alt="Sample image" />
-          </div>
-          <div className="md:w-1/3 max-w-sm">
-            <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder="Email Address" />
-            <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" />
-            <div className="mt-4 flex justify-between font-semibold text-sm">
-              <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
-                <input className="mr-1" type="checkbox" />
-                <span>Remember Me</span>
-              </label>
-              <a className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4" href="#">Forgot Password?</a>
-            </div>
-            <div className="text-center md:text-left">
-              <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider" type="submit">Login</button>
-            </div>
-            <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
-              Don't have an account? <a className="text-red-600 hover:underline hover:underline-offset-4" href="#">Register</a>
-            </div>
-          </div>
-        </section>
-      );
-    
+  const isMobile = window.innerWidth < 768;
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: SigninAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  return (
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+      <div className="md:w-1/2 max-w-lg">
+        <Lottie 
+          options={defaultOptions}
+          height={isMobile ? 250 : 500}
+          width={isMobile ? 350 : 500}
+        />
+      </div>
+      <div className="md:w-1/2 max-w-sm">
+        <div className="my-4 font-semibold text-2xl text-slate-500 text-center md:text-left">
+          Sign-In to your account
+        </div>
+        <input className="text-xl w-full px-4 py-2 border border-solid border-gray-300 rounded-xl" type="text" placeholder="Email Address" />
+        <input className="text-xl w-full px-4 py-2 border border-solid border-gray-300 rounded-xl mt-4" type="password" placeholder="Password" />
+        <div className="mt-4 flex justify-between font-semibold text-sm">
+          <a className="text-blue-600 text-lg hover:text-blue-700 hover:underline hover:underline-offset-4" href="#">Forgot Password?</a>
+        </div>
+        <div className="text-center md:text-left font-bold">
+          <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded-xl tracking-wider text-base" type="submit">Sign-In</button>
+        </div>
+        <div className="mt-4 font-semibold text-lg text-slate-500 text-center md:text-left">
+          Don't have an account? <a className="text-red-600 hover:underline hover:underline-offset-4 font-bold" href="#">Sign-Up</a>
+        </div>
+      </div>
+    </section>
+  );
 }
