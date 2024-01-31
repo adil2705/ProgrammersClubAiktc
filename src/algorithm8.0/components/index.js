2fe93d8ddee4ff42333a5d815e17d51eeb99ca68
Deshmukh Jawad <115729743+jawaddeshmukh04@users.noreply.github.com>
2024-01-31 15:57:29 +0530

    added algo landing

---
+++ src/algorithm8.0/components/index.js
@@ -0,0 +1,28 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Clients from "./Clients";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+export {
+  Hero,
+  Navbar,
+  Clients,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas
+};
---
+++ src/algorithm8.0/constants/index.js
@@ -0,0 +1,280 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    airbnb,
+    binance,
+    coinbase,
+    dropbox,
+    one,
+    two,
+    three,
+    four
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Develoment",
+      icon: web,
+    },
+    {
+      title: "App Development",
+      icon: mobile,
+    },
+    {
+      title: "Machine Learning",
+      icon: backend,
+    },
+    {
+      title: "blockchain technology",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Event Registration",
+      company_name: "Starbucks",
+      icon: one,
+      iconBg: "#383E56",
+      date: "13th Feb 2024 - 20th Feb 2024",
+      points: [
+        "Participants have to register themselves and form a team of 4 members.",
+      ],
+    },
+    {
+      title: "Problem statement announcement",
+      company_name: "Tesla",
+      icon: two,
+      iconBg: "#E6DEDD",
+      date: "1st March 2024",
+      points: [
+        "Problem statements will be announce and team leader has to select on topic and start work on it.",
+      ],
+    },
+    {
+      title: "Team Reporting Day",
+      company_name: "Shopify",
+      icon: three,
+      iconBg: "#383E56",
+      date: "3rd March 2024",
+      points: [
+        "Each team has to report to AIKTC CAMPUS at 9:00 AM IST to 10:00 AM IST.",
+      ],
+    },
+    {
+      title: "Winner Announcement",
+      company_name: "Meta",
+      icon: four,
+      iconBg: "#E6DEDD",
+      date: "4th March 2024",
+      points: [
+        "Winner will be announce.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  export const clients = [
+    {
+      id: "client-1",
+      logo: airbnb,
+    },
+    {
+      id: "client-2",
+      logo: binance,
+    },
+    {
+      id: "client-3",
+      logo: coinbase,
+    },
+    {
+      id: "client-4",
+      logo: dropbox,
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
