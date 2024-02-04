f276ddfdb317dae559e559cb18bad5b15b4ce35b
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-04 14:41:24 +0530

    Delete src/algorithm8.0 directory

--- src/algorithm8.0/components/canvas/index.js
+++
@@ -1,7 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
-import AstronautCanvas from "./astronaut"
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, AstronautCanvas };
