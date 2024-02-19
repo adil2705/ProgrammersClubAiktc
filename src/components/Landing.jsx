1d5a79635f2f605e3174bff3e9bbc74b399b02e1
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-19 21:56:21 +0530

    Update Landing.jsx
    Added algorithm link to pc button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -30,8 +30,8 @@ export default function Landing() {
             className="  space-x-1 sm:mt-6"
           >
             <a
-              className="focus:outline-none inline-block transform rounded-lg my-6 bg-green-400 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-green-600 focus:ring focus:ring-green-400 focus:ring-opacity-50 focus:ring-offset-2 active:bg-green-400 sm:text-base"
-              href="/algorithm"
+              className="focus:outline-none inline-block transform rounded-lg my-6 bg-orange-600 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-green-600 focus:ring focus:ring-green-400 focus:ring-opacity-50 focus:ring-offset-2 active:bg-green-400 sm:text-base"
+              href="https://Algorithm8.aiktc.ac.in"
               target="_self"
             >
               Algoritim 8.O
