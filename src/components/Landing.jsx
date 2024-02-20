dadb3999ac95f27091167ae5dc67ca12132265ee
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-20 12:01:52 +0530

    Update Landing.jsx
    btn

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -26,8 +26,8 @@ export default function Landing() {
             The Programmers Club is a student-led initiative established in 2015 with the motto "For the students, By the students." Since our inception, we've been fostering a dynamic community where students passionate about programming unite to share knowledge and enthusiasm for all things tech.
           </p>
           {/* button  */}
-        <a href="https://algorithm8.aiktc.ac.in/" class="relative inline-block m-2 text-lg group">
-<span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out  rounded-lg group-hover:text-white">
+        <a href="https://algorithm8.aiktc.ac.in/" class="relative inline-block my-2 text-lg group">
+<span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-orange-600  rounded-lg group-hover:text-white">
 <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-orange-50"></span>
 <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-orange-600 group-hover:-rotate-180 ease"></span>
 <span class="relative">Algorithm 8.O</span>
