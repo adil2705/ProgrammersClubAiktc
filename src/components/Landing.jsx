131e486a1cabb7b24876a4be3961d3071cfa0a0f
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-20 11:53:40 +0530

    Update Landing.jsx
    added mordern btn

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -26,17 +26,14 @@ export default function Landing() {
             The Programmers Club is a student-led initiative established in 2015 with the motto "For the students, By the students." Since our inception, we've been fostering a dynamic community where students passionate about programming unite to share knowledge and enthusiasm for all things tech.
           </p>
           {/* button  */}
-          <div
-            className="  space-x-1 sm:mt-6"
-          >
-            <a
-              className="focus:outline-none inline-block transform rounded-lg my-6 bg-orange-600 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-green-600 focus:ring focus:ring-green-400 focus:ring-opacity-50 focus:ring-offset-2 active:bg-green-400 sm:text-base"
-              href="https://Algorithm8.aiktc.ac.in"
-              target="_self"
-            >
-              Algoritim 8.O
-            </a>
-          </div>
+        <a href="https://algorithm8.aiktc.ac.in/" class="relative inline-block text-lg group">
+<span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+<span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+<span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+<span class="relative">Algorithm 8.O</span>
+</span>
+<span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-orange-600 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</a>
         </div>
       </div>
       {/* largest view  */}
