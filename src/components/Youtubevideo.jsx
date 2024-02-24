87ff6b71d3dc14be31b83fb9e715ca62e72e3e56
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-24 15:45:09 +0530

    Update Youtubevideo.jsx

--- src/components/Youtubevideo.jsx
+++ src/components/Youtubevideo.jsx
@@ -11,7 +11,7 @@ export default function Youtubevideo() {
     <div
       data-aos="fade-up"
       data-aos-duration="1000"
-      className="w-8/10 mx-4  max-w-7xl flex-1 rounded-lg bg-green-600 p-2 shadow-xl sm:p-3 md:mx-auto md:mt-24 md:w-3/5"
+      className="w-8/10 mx-4  max-w-7xl flex-1 rounded-lg bg-orange-600 p-2 shadow-xl sm:p-3 md:mx-auto md:mt-24 md:w-3/5"
     >
       <div className="aspect-w-16 aspect-h-9 ">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/3V1PciBdUJM?si=T6RBEFZ5d2fqiHpZ&amp;start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
