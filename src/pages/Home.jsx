8c3e9cd428c59324e84a1468c90b70b48108c6c9
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-25 17:28:03 +0530

    Update Home.jsx
    change content and btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,20 +47,22 @@ export default function Home() {
             />
             <div className="p-10 xl:p-16">
               <div className=" mb-8">
-                <h1 className=" mb-4 w-full text-2xl font-semibold text-blue-900">
-                  Algorithm <span className=" text-blue-500">7.0</span>
+                <h1 className=" mb-4 w-full text-2xl font-semibold text-blue-600">
+                  <span classname="text-4xl font-semibold text-orange-500 ">A</span>lgorithm <span className=" text-orange-500">8.0</span>
                 </h1>
                 <p>
                 Algorithm hackathon is an event where participants come together to solve real world problems, basically in a competitive environment.
                 </p>
               </div>
               <div>
-                <a
-                  href="/eventdetails"
-                  className=" focus:outline-none mt-10 rounded-full bg-green-400 p-2 px-4  text-center font-semibold text-white shadow-lg transition-all duration-300 hover:bg-green-800 hover:shadow-none focus:ring"
-                >
-                  Register Here
-                </a>
+                <a href="https://algorithm8.aiktc.ac.in/" class="relative inline-block my-2 text-lg group">
+<span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-orange-600  rounded-lg group-hover:text-white">
+<span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-orange-50"></span>
+<span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-orange-600 group-hover:-rotate-180 ease"></span>
+<span class="relative">Algorithm 8.O</span>
+</span>
+<span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-orange-600 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</a>
               </div>
             </div>
           </div> 
