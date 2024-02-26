57f76f6ea2aded1af86aba7bc7bc174fab957c60
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-26 05:45:29 +0530

    Update Teams.jsx

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -24,41 +24,42 @@ export default function Teams() {
     AOS.refresh();
   }, []);
   return (
+    <div className=" bg-gray-100">
     <p className="title-font mb-20 uppercase font-bold text-center text-2xl font-medium text-gray-700 sm:text-3xl" >
               <span className="">Meet The Team</span>
               
             </p>
     
-<div class="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-  <div class="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+<div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div className="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
     <img src="./TeamPic/IMG-20240225-WA0047.jpg" alt="profile-picture" />
   </div>
-  <div class="p-6 text-center">
-    <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+  <div className="p-6 text-center">
+    <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
         Prof. Tabrez Khan
     </h4>
-    <p class="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
+    <p className="block bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
       HOD Computer Department 
     </p>
   </div>
-  <div class="flex justify-center gap-7 p-6 pt-2">
+  <div className="flex justify-center gap-7 p-6 pt-2">
     <a
       href="#facebook"
-      class="block bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
+      className="block bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
     >
-      <i class="fab fa-facebook" aria-hidden="true"></i>
+      <i className="fab fa-facebook" aria-hidden="true"></i>
     </a>
     <a
       href="#twitter"
-      class="block bg-gradient-to-tr from-light-blue-600 to-light-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
+      className="block bg-gradient-to-tr from-light-blue-600 to-light-blue-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
     >
-      <i class="fab fa-twitter" aria-hidden="true"></i>
+      <i className="fab fa-twitter" aria-hidden="true"></i>
     </a>
     <a
       href="#instagram"
-      class="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
+      className="block bg-gradient-to-tr from-purple-600 to-purple-400 bg-clip-text font-sans text-xl font-normal leading-relaxed text-transparent antialiased"
     >
-      <i class="fab fa-instagram" aria-hidden="true"></i>
+      <i className="fab fa-instagram" aria-hidden="true"></i>
     </a>
   </div>
 </div>
@@ -77,7 +78,6 @@ export default function Teams() {
   integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
   crossorigin="anonymous"
 />
-    <div className=" bg-gray-100">
       <section className="  mx-auto   max-w-7xl overflow-hidden px-4 sm:px-6 ">
         <article className="col-auto mb-10 mt-10">
           <div
