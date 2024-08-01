aa32e564bfd3e600c6d4202f5e8e8c4215d234bd
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-08-01 11:42:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,11 +55,11 @@ export default function Home() {
                 </p>
               </div>
               <div>
-                <a href="https://algorithm8.aiktc.ac.in/" class="relative inline-block my-2 text-lg group">
+                <a href="https://programmersclubtest.netlify.app/" class="relative inline-block my-2 text-lg group">
 <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-orange-600  rounded-lg group-hover:text-white">
 <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-orange-50"></span>
 <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-orange-600 group-hover:-rotate-180 ease"></span>
-<span class="relative">Algorithm 8.O</span>
+<span class="relative">Recruitment</span>
 </span>
 <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-orange-600 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
 </a>
